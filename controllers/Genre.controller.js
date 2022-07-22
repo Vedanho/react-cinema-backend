0cc0b1ea29d8295c51537341fe43b62a58a22470
Amirhan Inasalamov <amirhan.inasalamov@mail.ru>
2022-07-22 17:55:23 +0300

    Добавил модели для фильмов и жанров. А также создал для них контроллеры и роуты.

--- controllers/Genre.controller.js
+++ controllers/Genre.controller.js
@@ -1,1 +1,44 @@
-console.log();
+const Genre = require("../models/Genre.model");
+
+module.exports.genreControllers = {
+  createGenre: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      const genre = await Genre.create({
+        name,
+      });
+
+      return res.json(genre);
+    } catch (error) {
+      return res.status(401).json(error.message);
+    }
+  },
+  getGenre: async (req, res) => {
+    try {
+      const genres = await Genre.find();
+      return res.json(genres);
+    } catch (error) {
+      return res.json(error.message);
+    }
+  },
+  updateGenre: async (req, res) => {
+    try {
+      const newGenre = await Genre.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+      });
+
+      return res.json(newGenre);
+    } catch (error) {
+      return res.status(401).json(error.message);
+    }
+  },
+  deleteGenre: async (req, res) => {
+    try {
+      await Genre.findByIdAndRemove(req.params.id);
+      return res.json("Жанр удалён");
+    } catch (error) {
+      return res.status(401).json(error.message);
+    }
+  },
+};
--- routes/Movie.route.js
+++ routes/Movie.route.js
@@ -1,1 +1,12 @@
-console.log();
+const { Router } = require("express");
+const { movieControllers } = require("../controllers/movies.controller");
+
+const router = Router();
+
+router.post("/movie", movieControllers.createMovie);
+router.get("/movie", movieControllers.getMovies);
+router.get("/movie/:id", movieControllers.getMoviesByGenre);
+router.patch("/movie/:id", movieControllers.updateMovie);
+router.delete("/movie/:id", movieControllers.deleteMovie);
+
+module.exports = router
