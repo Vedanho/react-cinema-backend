c2a83df187ccd896a965371f7e875b7db547527f
khald <abv.khallid@mail.ru>
2022-07-22 16:36:42 +0300

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+const cors = require("cors")
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+require("dotenv").config()
+
+const path = require("path")
+
+app.use(express.json())
+app.use(cors())
+
+app.use(require("./routes/User.route"))
+app.use(require("./routes/Genre.route"))
+app.use(require("./routes/Hall.route"))
+app.use(require("./routes/Movie.route"))
+app.use(require("./routes/Review.route"))
+
+mongoose
+  .connect(process.env.MONGO_SERVER)
+  .then(() => console.log("Успешно соединились с сервером MongoDB"))
+  .catch(() => console.log("Ошибка при соединении с сервером MongoDB"))
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+})
