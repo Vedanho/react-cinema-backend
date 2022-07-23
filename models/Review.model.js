5c8112d42a208767384d833b9e7dda5ca4b6c5a9
Khamzat Dasaev <home@MacBook-ameenuss.local>
2022-07-23 13:43:05 +0300

    Добавил модель/роут/контроллер для отзывов

--- models/Review.model.js
+++ models/Review.model.js
@@ -1,1 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+const reviewSchema = mongoose.Schema({
+  text: String,
+    movies: {
+      ref: "Movie",
+      typeof: mongoose.SchemaTypes.ObjectId,
+    },
+});
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
--- routes/User.route.js
+++ routes/User.route.js
@@ -1,1 +1,11 @@
-console.log()
+const { Router } = require("express");
+const { reviewsController } = require("../controllers/review.controller");
+
+const router = Router();
+
+router.get("/reviews", reviewsController.getAllReviews);
+router.post("/review", reviewsController.postReview);
+router.patch("/review/:id", reviewsController.patchReviewById);
+router.delete("/review/:id", reviewsController.delReviewById);
+
+module.exports = router;
