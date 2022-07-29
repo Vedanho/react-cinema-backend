e0899de8e5558b1a1849f6d9a1826d69c4772b97
khald <abv.khallid@mail.ru>
2022-07-29 18:52:01 +0300

    fix bags

--- index.js
+++ index.js
@@ -1,75 +1,55 @@
-const cors = require("cors");
-const express = require("express");
-const mongoose = require("mongoose");
+const cors = require("cors")
+const express = require("express")
+const mongoose = require("mongoose")
 
-const app = express();
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
-const errorMiddlewares = require("./middlewares/error-middlewares");
+const app = express()
+const cookieParser = require("cookie-parser")
+require("dotenv").config()
+const errorMiddlewares = require("./middlewares/error-middlewares")
+const path = require("path")
 
-app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
-app.use(require("./routes/User.route"));
-app.use(errorMiddlewares);
+app.use(errorMiddlewares)
 
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
 
-const mail = require("./mail.js");
+const mail = require("./mail.js")
 
-const app = express();
-require("dotenv").config();
-
-const path = require("path");
-
-app.use(express.json());
-app.use(cors());
-
-
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get("/", (req, res) =>
   res.send(`Requested from ${req.hostname} : <h1>Hello World</h1>`)
-);
+)
+
 app.post("/mail", async (req, res) => {
-  const { email, message } = req.body;
+  const { email, message } = req.body
 
-  mail("root@localhost.ru", email, "subj", message);
+  mail("root@localhost.ru", email, "subj", message)
 
-  return res.json("отправлено");
-});
+  return res.json("отправлено")
+})
 
-app.use(require("./routes/User.route"));
-app.use(require("./routes/Genre.route"));
-app.use(require("./routes/Hall.route"));
-app.use(require("./routes/Movie.route"));
-app.use(require("./routes/Review.route"));
-const app = express();
-const multer = require("multer")
-require("dotenv").config();
+app.use(require("./routes/User.route"))
 
-const path = require("path");
-
-app.use(express.json());
-app.use(cors());
-
-app.use(require("./routes/Cinema.route"));
-app.use(require("./routes/User.route"));
-app.use(require("./routes/Genre.route"));
-app.use(require("./routes/Hall.route"));
-app.use(require("./routes/Movie.route"));
-app.use(require("./routes/Book.route"));
-app.use(require("./routes/Session.route"));
+app.use(require("./routes/Cinema.route"))
+app.use(require("./routes/User.route"))
+app.use(require("./routes/Genre.route"))
+app.use(require("./routes/Hall.route"))
+app.use(require("./routes/Movie.route"))
+app.use(require("./routes/Book.route"))
+app.use(require("./routes/Session.route"))
 // app.use(require("./routes/Review.route"))
 
-app.use("/images", express.static(path.resolve(__dirname, "images")));
+app.use("/images", express.static(path.resolve(__dirname, "images")))
 
 mongoose
   .connect(process.env.MONGO_SERVER)
   .then(() => console.log("Успешно соединились с сервером MongoDB"))
-  .catch(() => console.log("Ошибка при соединении с сервером MongoDB"));
+  .catch((e) => console.log(e.message))
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
-});
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+})
--- routes/User.route.js
+++ routes/User.route.js
@@ -1,32 +1,28 @@
-const Router = require("express");
-const { userController } = require("../controllers/User.controller");
-const { body } = require("express-validator");
-const authMiddleware = require("../middlewares/auth.middlewares");
+const Router = require("express")
+const { userController } = require("../controllers/User.controller")
+const { body } = require("express-validator")
+const authMiddleware = require("../middlewares/auth.middlewares")
+const { reviewsController } = require("../controllers/review.controller")
 
-const router = Router();
+const router = Router()
 
-router.patch("/users/:id", userController.patchController);
-router.delete("/users/:id", userController.deleteController);
+router.patch("/users/:id", userController.patchController)
+router.delete("/users/:id", userController.deleteController)
 router.post(
   "/registration",
   body("email").isEmail(),
   body("password").isLength({ min: 6, max: 32 }),
   userController.userRegistrationController
-);
-router.post("/logout", userController.logout);
-router.post("/login", userController.login);
-router.get("/activated/:link", userController.activateController);
-router.get("/refresh", userController.refresh);
-router.get("/getUsers", authMiddleware, userController.getUsers);
-=======
-const { Router } = require("express");
-const { reviewsController } = require("../controllers/review.controller");
+)
+router.post("/logout", userController.logout)
+router.post("/login", userController.login)
+router.get("/activated/:link", userController.activateController)
+router.get("/refresh", userController.refresh)
+router.get("/getUsers", authMiddleware, userController.getUsers)
 
-const router = Router();
+router.get("/reviews", reviewsController.getAllReviews)
+router.post("/review", reviewsController.postReview)
+router.patch("/review/:id", reviewsController.patchReviewById)
+router.delete("/review/:id", reviewsController.delReviewById)
 
-router.get("/reviews", reviewsController.getAllReviews);
-router.post("/review", reviewsController.postReview);
-router.patch("/review/:id", reviewsController.patchReviewById);
-router.delete("/review/:id", reviewsController.delReviewById);
-
-module.exports = router;
+module.exports = router
