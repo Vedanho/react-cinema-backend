5335d3ae765d656af2cfcbad067e773d44a08102
AgReSSoR <samahkinets@gmail.com>
2022-07-29 18:04:41 +0300

    Сделал Авторизация продвинутое

---
+++ exceptions/api-error.js
@@ -0,0 +1,16 @@
+module.exports = class ApiError extends Error {
+  status;
+  errors;
+
+  constructor(status, message, errors = []) {
+    super(message);
+    this.status = status;
+    this.errors = errors;
+  }
+  static UnauthorizedError() {
+    return new ApiError(401, "Пользователь не авторизован");
+  }
+  static BodRequest(message, error = []) {
+    return new ApiError(400, message, error);
+  }
+};
--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const cors = require("cors")
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express()
-require("dotenv").config()
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const errorMiddlewares = require("./middlewares/error-middlewares");
 
-const path = require("path")
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(cors())
-
-app.use(require("./routes/User.route"))
-app.use(require("./routes/Genre.route"))
-app.use(require("./routes/Hall.route"))
-app.use(require("./routes/Movie.route"))
-app.use(require("./routes/Review.route"))
+app.use(require("./routes/User.route"));
+app.use(errorMiddlewares);
 
 mongoose
   .connect(process.env.MONGO_SERVER)
   .then(() => console.log("Успешно соединились с сервером MongoDB"))
-  .catch(() => console.log("Ошибка при соединении с сервером MongoDB"))
+  .catch(() => console.log("Ошибка при соединении с сервером MongoDB"));
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
